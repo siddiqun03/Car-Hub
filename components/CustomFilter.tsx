2a9cc1f2d12041fa1a3bc135a7c89b90a2b34419
siddiqun03 <siddiqun.dev@gmail.com>
2023-07-25 19:31:46 +0500

    initial commit & navbar, hero & footer done!

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const CustomFilter = () => {
+	return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const SearchBar = () => {
+	return <div>SearchBar</div>;
+};
+
+export default SearchBar;
